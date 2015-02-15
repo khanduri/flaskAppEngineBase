3195865b1b01de98603ef5e05403f7988f2ad153
Prashant Khanduri <pkhanduri@jawbone.com>
2015-02-14 18:04:27 -0800

    json decorator base

--- src/application/compute/api.py
+++ src/application/compute/api.py
@@ -1,4 +1,4 @@
-import application.base.api
+import application.base.decorators
 import application.compute.forms
 import application.compute.services
 
@@ -7,24 +7,26 @@ import application.compute.services
 # Collection operations
 ##########################################
 
-
+@application.base.decorators.return_json
 def get_all_computes():
     computes = application.compute.services.fetch_all_computes()
     compute_list = [e.get_dict_repr() for e in computes]
-    return application.base.api.get_json_packet(data=compute_list)
+    return 200, compute_list
 
 
+@application.base.decorators.return_json
 def post_compute():
     form = application.compute.forms.ComputeForm(csrf_enabled=False)
     if form.validate_on_submit():
         application.compute.services.create_new_compute(form.data.get('a'), form.data.get('b'))
-        return application.base.api.get_json_packet()
-    return application.base.api.get_json_packet(status=404)
+        return 200, {}
+    return 404, {}
 
 
+@application.base.decorators.return_json
 def delete_all_computes():
     application.compute.services.remove_all_computes()
-    return application.base.api.get_json_packet()
+    return 200, {}
 
 
 ##########################################
@@ -32,20 +34,22 @@ def delete_all_computes():
 ##########################################
 
 
+@application.base.decorators.return_json
 def get_compute(compute_id):
     compute_instance = application.compute.services.fetch_compute(compute_id)
-    return application.base.api.get_json_packet(data=compute_instance.get_dict_repr())
+    return 200, compute_instance.get_dict_repr()
 
 
+@application.base.decorators.return_json
 def update_compute(compute_id):
     form = application.compute.forms.ComputeForm(csrf_enabled=False)
     if form.validate_on_submit():
         compute_instance = application.compute.services.modify_compute(compute_id, form.data.get('a'), form.data.get('b'))
-        return application.base.api.get_json_packet(data=compute_instance.get_dict_repr())
-    return application.base.api.get_json_packet(status=404)
+        return 200, compute_instance.get_dict_repr()
+    return 404, {}
 
 
+@application.base.decorators.return_json
 def delete_compute(compute_id):
     application.compute.services.remove_compute(compute_id)
-    return application.base.api.get_json_packet()
-
+    return 200, {}
