c7d92ea26152e32e016d8f6c5f928ad4e9e8a975
Prashant Khanduri <pkhanduri@jawbone.com>
2015-02-24 22:54:19 -0800

    Adding reset email template

--- src/application/user/mails.py
+++ src/application/user/mails.py
@@ -2,13 +2,39 @@ import flask
 import application.core.mails
 
 
+def send_reset(email, first, reset_code):
+    send_to = [email]
+    subject = "Account reset instructions"
+
+    data = {
+        'first': first.capitalize(),
+        'link': '%s?reset_code=%s' % (flask.url_for('.reset', _external=True), reset_code)
+    }
+
+    body = """
+    Hello %(first)s!
+
+    To reset your account, please click on the link below.
+    %(link)s
+
+    If for some reason the link doesn't work, copy the link manually and visit the page.
+
+    Regards
+    """ % data
+
+    html = body
+    text = body
+
+    application.core.mails.send_message(send_to, subject, html, text)
+
+
 def send_verification(email, first, verification_code):
     send_to = [email]
     subject = "Required | Account verification instructions"
 
     data = {
         'first': first.capitalize(),
-        'link': flask.url_for('/verify', data={'verification_code': verification_code})
+        'link': '%s?verification_code=%s' % (flask.url_for('.verify', _external=True), verification_code)
     }
 
     body = """
